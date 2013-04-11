23087be1221d096b21d11b9441f209abd80c57b5
Don Najd <dnajd7@gmail.com>
2013-04-11 00:43:19 -0700

    better naming conventions

--- src/main/db_create.py
+++ src/main/db_create.py
@@ -4,14 +4,15 @@ from dynamodb_mapper.model import DynamoDBModel
 from dynamodb_mapper.model import ConnectionBorg
 from ddbmock import config
 from ddbmock import connect_boto_patch
-from test_map import TestMap
+from db_models import TestMap
 
-###########################
-# ddbmock
+# mock
 config.STORAGE_ENGINE_NAME = 'sqlite'                   # switch to sqlite backend
 config.STORAGE_SQLITE_FILE = '/tmp/pyrest.sqlite'       # define the database path. defaults to 'dynamo.db'
 db = connect_boto_patch()                               # Wire-up boto and ddbmock together
 
-# Dynamo DB Connection
+# connect
 conn = ConnectionBorg()
+
+# create table
 conn.create_table(TestMap, 10, 10) #, wait_for_active=True) # create table
--- src/main/rest_test.py
+++ src/main/rest_test.py
@@ -4,13 +4,43 @@ from dynamodb_mapper.model import DynamoDBModel
 from dynamodb_mapper.model import ConnectionBorg
 from ddbmock import config
 from ddbmock import connect_boto_patch
+from flask import Flask
+from db_models import TestMap
 
 ###########################
-# Dynamo DB Model
-class TestMap(DynamoDBModel):
-    __table__ = u"test_map"
-    __hash_key__ = u"test_key"
-    __schema__ = {
-        u"test_key": int,
-        u"name": unicode,
-    }
+# ddbmock
+
+config.STORAGE_ENGINE_NAME = 'sqlite'                   # switch to sqlite backend
+config.STORAGE_SQLITE_FILE = '/tmp/pyrest.sqlite'       # define the database path. defaults to 'dynamo.db'
+db = connect_boto_patch()                               # Wire-up boto and ddbmock together
+
+###########################
+# Flash routes
+
+# create app
+app = Flask(__name__)
+
+# POST: /test/new
+@app.route('/test/new', methods=['GET'])
+def post_test():
+    t1 = TestMap()
+    t1.test_key = 1
+    t1.name = u"Hello World"
+    t1.save()
+    return "success"
+
+
+# GET: /test/int:id
+@app.route('/test/<int:id>', methods=['GET'])
+def get_test(id):
+    t1 = TestMap.get(id)
+    return t1.name
+
+# connect and run
+if __name__ == '__main__':
+
+    # Dynamo DB Connection
+    conn = ConnectionBorg()
+
+    # run app
+    app.run(debug=True)

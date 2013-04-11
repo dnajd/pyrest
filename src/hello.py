aba6e017836c8f6307544521897c21ace0054d7d
Don Najd <dnajd7@gmail.com>
2013-04-10 22:51:42 -0700

    added example of status code failure

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def hello_world():
     return 'Hello World!'
+
+@app.route('/fail')
+def login():
+    abort(401)
 
 if __name__ == '__main__':
     app.run()
